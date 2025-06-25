afccbabc87984ef912bd918e47878cc77ae49e26
zafkiel45 <zafkielc@gmail.com>
2025-06-25 14:18:01 -0300

    chore(db): deleted uncessary folders

--- database/migration.database.ts
+++ database/migration.database.ts
@@ -1,4 +1,4 @@
-import { db } from "../config/config.database";
+import { db } from "./config.database";
 import { sql } from "bun";
 
 await db.connect()
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 import './database/migrations/migration.database';
+
+const server = Bun.serve({
+  port: 3000,
+  idleTimeout: 0,
+  routes: {
+    "/api/shorturl/": {
+
+    },
+  },
+})
