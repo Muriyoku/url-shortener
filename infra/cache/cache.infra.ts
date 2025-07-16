97663ac688838dc4780745c15376705b0d1be3e3
zafkiel45 <zafkielc@gmail.com>
2025-07-16 15:28:08 -0300

    refatore(infra): reorganized statements on cache file

--- infra/cache/cache.infra.ts
+++ infra/cache/cache.infra.ts
@@ -8,10 +8,16 @@ const cache: Map<string, cacheType> = new Map();
 
 export function getFromCache(code: string) {
   const cachedValue = cache.get(code);
+
   if(cachedValue) {
-    if(cachedValue.expiration < Date.now()) {cache.delete(code)}; 
+    if(cachedValue.expiration < Date.now()) {
+      cache.delete(code)
+    }; 
+
     return cachedValue;
-  } else return null;
+  } else {
+    return null
+  };
 };
 
 export function addToCache(key: string, value: string | undefined | null) {
